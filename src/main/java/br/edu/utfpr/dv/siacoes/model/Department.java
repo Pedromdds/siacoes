d133da1d9bcc4f218447d8fa9eaf7f63f87bd55c
Pedromdds <30834661+Pedromdds@users.noreply.github.com>
2020-09-02 10:26:50 -0300

    Lombok commit
    O Lombok é uma biblioteca Java focada em produtividade e redução de código boilerplate que por meio de anotações adicionadas ao nosso código ensinamos o compilador (maven ou gradle) durante o processo de compilação a criar código Java

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
@@ -1,82 +1,24 @@
-﻿package br.edu.utfpr.dv.siacoes.model;
-
+package br.edu.utfpr.dv.siacoes.model;
+import lombok.Data;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Department implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private int idDepartment;
-	private Campus campus;
-	private String name;
-	private String fullName;
-	private transient byte[] logo;
-	private boolean active;
-	private String site;
-	private String initials;
-	
-	public Department(){
-		this.setIdDepartment(0);
-		this.setCampus(new Campus());
-		this.setName("");
-		this.setFullName("");
-		this.setLogo(null);
-		this.setActive(true);
-		this.setSite("");
-		this.setInitials("");
-	}
-	
-	public int getIdDepartment() {
-		return idDepartment;
-	}
-	public void setIdDepartment(int idDepartment) {
-		this.idDepartment = idDepartment;
-	}
-	public Campus getCampus() {
-		return campus;
-	}
-	public void setCampus(Campus campus) {
-		this.campus = campus;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getFullName(){
-		return fullName;
-	}
-	public void setFullName(String fullName){
-		this.fullName = fullName;
-	}
-	public byte[] getLogo() {
-		return logo;
-	}
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
-	public boolean isActive() {
-		return active;
-	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getSite(){
-		return site;
-	}
-	public void setSite(String site){
-		this.site = site;
-	}
-	public String getInitials() {
-		return initials;
-	}
-	public void setInitials(String initials) {
-		this.initials = initials;
-	}
-	
-	public String toString(){
-		return this.getName();
-	}
-	
+import br.edu.utfpr.dv.siacoes.util.DateUtils;
+
+public @Data class ActivitySubmissionReport implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String student;
+    private String studentCode;
+    private int registerSemester;
+    private int registerYear;
+    private double totalScore;
+    private String situation;
+    private List<ActivitySubmissionDetailReport> details;
+    private List<ActivitySubmissionFooterReport> footer;
+
+
+
 }
